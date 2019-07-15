668f0f52bbeb196a5d1afd02bbf72ca0e1f4e18f
rodilo <shonely@gmail.com>
2019-07-15 11:40:31 +0200

    Set clean project

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,11 +1,7 @@
 import React from 'react'
-import { navigate } from '@reach/router'
 
-export default () => { 
-  navigate('/blog')
-  return (
+export default () => (
   <div>
     <p>React Static is a progressive static site generator for React.</p>
   </div>
 )
-  }
