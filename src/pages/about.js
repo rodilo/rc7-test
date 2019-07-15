676076e9f77b8cc18c3023a4960881de372047e0
rodilo <shonely@gmail.com>
2019-07-15 11:01:22 +0200

    init commit

---
+++ src/pages/about.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { navigate } from '@reach/router'
+
+export default () => { 
+  navigate('/blog')
+  return (
+  <div>
+    <p>React Static is a progressive static site generator for React.</p>
+  </div>
+)
+  }
