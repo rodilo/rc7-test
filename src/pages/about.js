a3c73aa7b975345c96786edf502de1c91acb7fc9
rodilo <shonely@gmail.com>
2019-07-15 13:00:04 +0200

    Test navigate on ie11

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,7 +1,11 @@
-import React from 'react'
+import React from "react";
+import { navigate } from "@reach/router";
 
-export default () => (
-  <div>
-    <p>React Static is a progressive static site generator for React.</p>
-  </div>
-)
+export default () => {
+  navigate("/blog");
+  return (
+    <div>
+      <p>React Static is a progressive static site generator for React.</p>
+    </div>
+  );
+};
